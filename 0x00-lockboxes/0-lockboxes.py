0b3f6d0c912627463117857d8ae70b52a849ebcc
fabio-gz <fabiogomez.silva@gmail.com>
2020-12-10 21:46:57 -0500

    [add]boxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """checks if all boxes can be opened."""
+
+    stack = [0]
+    unlocked = set(stack)
+
+    while stack:
+        i = stack.pop()
+        for j in boxes[i]:
+            if j not in unlocked:
+                stack.append(j)
+                unlocked.add(j)
+            if len(unlocked) == len(boxes):
+                return True
+
+    return False
