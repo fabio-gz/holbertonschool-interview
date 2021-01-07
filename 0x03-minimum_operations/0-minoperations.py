73638eb22a011688db876fa1246b4400d45a2f57
fabio-gz <fabiogomez.silva@gmail.com>
2021-01-06 21:42:28 -0500

    [fix]exception

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-Minimun operations nedeed 
+Minimun operations nedeed
 """
 
 
 def minOperations(n):
+    if (n < 2):
+        return 0
+
     op = 0
     for i in range(2, n):
         while(n % i == 0):
