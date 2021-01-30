6db37fd50031c06ff553bec492d882395304b496
fabio-gz <fabiogomez.silva@gmail.com>
2021-01-30 11:28:02 -0500

    [add]executable

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'a script that reads stdin line by line and computes metrics, size'
+'a script that reads stdin line by line and computes metrics size'
 import sys
 import traceback
 
